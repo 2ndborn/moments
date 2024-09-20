97046e2eae789347fdb30bf2233d7f4cb6ca5773
Micah Francis <micah.francis69@googlemail.com>
2024-09-20 05:52:45 +0000

    Added post function to post

--- src/pages/posts/PostCreateForm.js
+++ src/pages/posts/PostCreateForm.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
@@ -13,6 +13,8 @@ import appStyles from "../../App.module.css";
 import btnStyles from "../../styles/Button.module.css";
 import Asset from "../../components/Asset";
 import { Image } from "react-bootstrap";
+import { useHistory } from "react-router-dom";
+import { axiosReq } from "../../api/axiosDefaults";
 
 function PostCreateForm() {
   const [postData, setPostData] = useState({
@@ -21,6 +23,9 @@ function PostCreateForm() {
     image: '',
   })
   const { title, content, image } = postData
+
+  const imageInput = useRef(null);
+  const history = useHistory();
 
   const [errors, setErrors] = useState({});
 
@@ -38,6 +43,25 @@ function PostCreateForm() {
         ...postData,
         image: URL.createObjectURL(event.target.files[0]),
       });
+    }
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    const formData = new FormData();
+
+    formData.append("title", title);
+    formData.append("content", content);
+    formData.append("image", imageInput.current.files[0]);
+
+    try {
+      const { data } = await axiosReq.post("/posts/", formData);
+      history.push(`/posts/${data.id}`);
+    } catch (err) {
+      console.log(err);
+      if (err.response?.status !== 401) {
+        setErrors(err.response?.data);
+      }
     }
   };
 
@@ -66,7 +90,7 @@ function PostCreateForm() {
 
       <Button
         className={`${btnStyles.Button} ${btnStyles.Blue}`}
-        onClick={() => {}}
+        onClick={() => history.goBack()}
       >
         cancel
       </Button>
@@ -77,7 +101,7 @@ function PostCreateForm() {
   );
 
   return (
-    <Form>
+    <Form onSubmit={handleSubmit}>
       <Row>
         <Col className="py-2 p-0 p-md-2" md={7} lg={8}>
           <Container
@@ -103,7 +127,9 @@ function PostCreateForm() {
                   className="d-flex justify-content-center"
                   htmlFor="image-upload"
                 >
-                  <Asset src={Upload} message="Click or tap to upload an image" />
+                  <Asset 
+                  src={Upload} 
+                  message="Click or tap to upload an image" />
                 </Form.Label>
               )}
               
@@ -111,6 +137,7 @@ function PostCreateForm() {
                 id="image-upload" 
                 accept="image/*" 
                 onChange={handleChangeImage} 
+                ref={imageInput}
                 />
 
             </Form.Group>
