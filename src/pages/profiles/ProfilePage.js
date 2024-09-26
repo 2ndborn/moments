ab88dd2990216d28320be9c4075dda6bbc857d59
Micah Francis <micah.francis69@googlemail.com>
2024-09-26 06:01:50 +0000

    Added follow functionality

--- src/pages/profiles/ProfilePage.js
+++ src/pages/profiles/ProfilePage.js
@@ -25,7 +25,7 @@ function ProfilePage() {
   const [hasLoaded, setHasLoaded] = useState(false);
   const currentUser = useCurrentUser();
   const {id} = useParams();
-  const setProfileData = useSetProfileData();
+  const {setProfileData, handleFollow} = useSetProfileData();
   const { pageProfile } = useProfileData();
   const [profile] = pageProfile.results;
   const is_owner = currentUser?.username === profile?.owner;
@@ -87,7 +87,7 @@ function ProfilePage() {
                 className={`
                     ${btnStyles.Button} 
                     ${btnStyles.Black}`} 
-                    onClick={() => {}}
+                    onClick={() => handleFollow(profile)}
                     >
                         follow
                     </Button>
